2f5bc647759257696919a04ca3aaf27c88615873
Frying Pan <tryteksebastian@gmail.com>
2023-02-26 19:21:53 +0100

    added some styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam neque
+        quasi deserunt modi excepturi aliquid expedita animi aspernatur vero
+        illo.
+      </p>
+    </section>
   );
 };
 
